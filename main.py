c9f86645a556b4ec018f089094f876bbc4b6297e
tudorobretin <tudorobre@gmail.com>
2022-04-06 09:39:50 +0300

    general outline of project logic

---
+++ main.py
@@ -0,0 +1,8 @@
+# accept cookies
+# press 50 per page
+# get number of pages
+# feed this number of pages into get article links functions' while
+# make a function to get all article links from 200+ pages
+# make another function which takes all links from previous function and access each link
+# check to see if ioana's phone is in there
+# if it is, show the link

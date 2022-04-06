3401431f7a41e5dd6b4a7512ad621c7031f4883c
tudorobretin <tudorobre@gmail.com>
2022-04-06 09:59:45 +0300

    added main structure of Get class

---
+++ get.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+
+
+class Get:
+    def __init__(self):
+        self.init = 0
+
+
+    def article_links(self):
+        pass
+
+    def phone_numbers(self):
+        pass
+
+
+

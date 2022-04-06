1f18c45b1ae824e4e8e5e1537fd69499892a7b6f
tudorobretin <tudorobre@gmail.com>
2022-04-06 10:11:19 +0300

    added part feature that gets number of pages

--- get.py
+++ get.py
@@ -10,7 +10,30 @@ class Get:
 
 
     def article_links(self):
-        pass
+
+        def get_number_of_pages():
+            driver = webdriver.Chrome(
+                executable_path="C:\\Users\\MAXMEDIA\\Desktop\\Python downloads\\Chromedriver\\chromedriver.exe")
+            URL = "https://www.publi24.ro/anunturi/matrimoniale/"
+            driver.get(URL)
+            time.sleep(1)
+
+            over_18_accept = driver.find_element(By.XPATH, '/html/body/div[10]/div/div/div/a[2]')
+            over_18_accept.click()
+
+            accept_cookies = driver.find_element(By.XPATH, '/html/body/div[17]/div/div/a[1]')
+            accept_cookies.click()
+
+            results_per_page = driver.find_element(By.XPATH, '/html/body/div[11]/div/div/div[4]/ul[1]/li[1]/p/span[2]')
+            results_per_page.click()
+
+            number_of_pages = int(driver.find_element(By.XPATH, '/html/body/div[10]/div/div/div[4]/ul[2]/li[11]/a').text)
+            return number_of_pages
+
+        pages = get_number_of_pages()
+        print(pages)
+
+        time.sleep(100)
 
     def phone_numbers(self):
         pass
--- main.py
+++ main.py
@@ -7,19 +7,11 @@
 # check to see if ioana's phone is in there
 # if it is, show the link
 from get import Get
-from check import Check
 
 get = Get()
-check = Check()
 
 article_links =[]
 article_links = get.article_links()
 
 phone_numbers = []
 phone_numbers = get.phone_numbers()
-
-match = check.number()
-
-if match:
-    link = check.link()
-    print(link)

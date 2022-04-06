0943526c177a87f42c23da27ce71223ca7dda248
tudorobretin <tudorobre@gmail.com>
2022-04-06 11:07:48 +0300

    finished article_links function which gets all links from publi24

--- get.py
+++ get.py
@@ -10,30 +10,36 @@ class Get:
 
 
     def article_links(self):
+        driver = webdriver.Chrome(
+            executable_path="C:\\Users\\MAXMEDIA\\Desktop\\Python downloads\\Chromedriver\\chromedriver.exe")
+        URL = "https://www.publi24.ro/anunturi/matrimoniale/"
+        driver.get(URL)
+        time.sleep(1)
 
-        def get_number_of_pages():
-            driver = webdriver.Chrome(
-                executable_path="C:\\Users\\MAXMEDIA\\Desktop\\Python downloads\\Chromedriver\\chromedriver.exe")
-            URL = "https://www.publi24.ro/anunturi/matrimoniale/"
-            driver.get(URL)
-            time.sleep(1)
+        over_18_accept = driver.find_element(By.XPATH, '/html/body/div[10]/div/div/div/a[2]')
+        over_18_accept.click()
 
-            over_18_accept = driver.find_element(By.XPATH, '/html/body/div[10]/div/div/div/a[2]')
-            over_18_accept.click()
+        accept_cookies = driver.find_element(By.XPATH, '/html/body/div[17]/div/div/a[1]')
+        accept_cookies.click()
 
-            accept_cookies = driver.find_element(By.XPATH, '/html/body/div[17]/div/div/a[1]')
-            accept_cookies.click()
+        results_per_page = driver.find_element(By.XPATH, '/html/body/div[11]/div/div/div[4]/ul[1]/li[1]/p/span[2]')
+        results_per_page.click()
 
-            results_per_page = driver.find_element(By.XPATH, '/html/body/div[11]/div/div/div[4]/ul[1]/li[1]/p/span[2]')
-            results_per_page.click()
+        number_of_pages = int(driver.find_element(By.XPATH, '/html/body/div[10]/div/div/div[4]/ul[2]/li[11]/a').text)
+        print(number_of_pages)
+        links = []
 
-            number_of_pages = int(driver.find_element(By.XPATH, '/html/body/div[10]/div/div/div[4]/ul[2]/li[11]/a').text)
-            return number_of_pages
+        for page in range(1, number_of_pages - 1):
+            #time.sleep(1)
+            print(page)
+            article_titles = driver.find_elements(By.CSS_SELECTOR, '.article-title a')
+            for title in article_titles:
+                link = title.get_attribute('href')
+                links.append(link)
+            next_page = driver.find_elements(By.CLASS_NAME, 'arrow')[1]
+            next_page.click()
 
-        pages = get_number_of_pages()
-        print(pages)
-
-        time.sleep(100)
+        return links
 
     def phone_numbers(self):
         pass
